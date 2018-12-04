0c1abce56e49ddc2376e313ed8709e0da947fb98
Bianca A <frank@Bianca-Avila-4.cablecolor.hn>
2018-12-04 16:24:14 -0600

    nav

--- main.js
+++ main.js
@@ -1,14 +1,12 @@
 $(()=>{
-	console.log('works');
+
 	var home_class= document.getElementById('home').querySelectorAll('img');
 
-console.log(home_class);
 	home_class.forEach(e=>{
 		$(e).on("mouseenter",()=>{
 			var  text= $(e).attr('alt')
 			e.style.opacity = "0.5"
 			e.style.cursor="pointer"
-			e.innerHTML += text;
 		})
 		$(e).on("mouseleave", ()=>{
 			e.style.opacity ="1"
@@ -16,7 +14,12 @@ console.log(home_class);
 	})
 	
 
-
+$(".upper-icon").on("click",()=>{
+	
+	var section = document.querySelector('.selection');
+	section.style.display='inline'
+	
+})
 
 })
 
