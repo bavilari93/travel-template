b3c830ae2af18eccdd65e7ff98de16b4a3d5dca4
Bianca A <frank@Bianca-Avila-4.cablecolor.hn>
2018-12-05 17:40:26 -0600

    hover fix

--- main.js
+++ main.js
@@ -1,24 +1,14 @@
 $(() => {
 
-    var home_class = document.getElementById('home').querySelectorAll('img');
+    var home_class = document.getElementById('home').querySelectorAll('.overlay');
     var overlay = document.querySelectorAll('span');
 
     home_class.forEach((e, index) => {
         var span = overlay[index]
         $(e).hover( () => {
-
-            
-            // var text = $(e).attr('alt');
-            // var span = document.createElement('span');
-            // span.innerHTML = text;
-            // overlayIndex.append(span);
             span.style.width="100%";
-            // e.style.opacity = "0.5"
-
-
         },() => {
-            span.style.width="0%";
-            
+            span.style.width="0%"; 
         })
 
     })
