999406cacd9740788f5f5f6533782ea36a9be001
Bianca A <frank@Bianca-Avila-4.cablecolor.hn>
2018-12-05 17:29:58 -0600

    over text

--- main.js
+++ main.js
@@ -1,33 +1,41 @@
-$(()=>{
+$(() => {
 
-	var home_class= document.getElementById('home').querySelectorAll('img');
+    var home_class = document.getElementById('home').querySelectorAll('img');
+    var overlay = document.querySelectorAll('span');
 
-	home_class.forEach(e=>{
-		$(e).on("mouseenter",()=>{
-			var  text= $(e).attr('alt')
-			e.style.opacity = "0.5"
-			e.style.cursor="pointer"
-		})
-		$(e).on("mouseleave", ()=>{
-			e.style.opacity ="1"
-		})
-	})
-	
+    home_class.forEach((e, index) => {
+        var span = overlay[index]
+        $(e).hover( () => {
 
-$(".upper-icon").on("click",()=>{
-	
-	var section = document.querySelector('.selection');
-	
-	if (section.style.display=='inline'){
-		section.style.display='none';
-		 $("html, body").animate({ scrollTop: 0 }, "slow");	
-	}else{
-		section.style.display='inline';
-		 
-	}
-	
+            
+            // var text = $(e).attr('alt');
+            // var span = document.createElement('span');
+            // span.innerHTML = text;
+            // overlayIndex.append(span);
+            span.style.width="100%";
+            // e.style.opacity = "0.5"
+
+
+        },() => {
+            span.style.width="0%";
+            
+        })
+
+    })
+
+
+    $(".upper-icon").on("click", () => {
+
+        var section = document.querySelector('.selection');
+
+        if (section.style.display == 'inline') {
+            section.style.display = 'none';
+            $("html, body").animate({ scrollTop: 0 }, "slow");
+        } else {
+            section.style.display = 'inline';
+
+        }
+
+    })
+
 })
-
-})
-
-
